0bd9a86dcbd3adbf4101a9c09e38a37853b28e49
endlessrequiem <austin.j.white.96@gmail.com>
2020-07-15 10:49:19 -0700

    finished number 11

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -2,6 +2,7 @@ package com.example.android.clippingexample
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -56,6 +57,35 @@ class ClippedView @JvmOverloads constructor(
         drawTranslatedTextExample(canvas)
         // drawQuickRejectExample(canvas)
     }
+
+    private fun drawClippedRectangle(canvas: Canvas) {
+        canvas.clipRect(
+            clipRectLeft, clipRectTop,
+            clipRectRight, clipRectBottom
+        )
+
+        canvas.drawColor(Color.WHITE)
+        paint.color = Color.RED
+        canvas.drawLine(
+            clipRectLeft, clipRectTop,
+            clipRectRight, clipRectBottom, paint
+        )
+
+        paint.color = Color.GREEN
+        canvas.drawCircle(
+            circleRadius, clipRectBottom - circleRadius,
+            circleRadius, paint
+        )
+
+        paint.color = Color.BLUE
+        paint.textSize = textSize
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText (
+            context.getString(R.string.clipping),
+            clipRectRight, textOffset, paint
+        )
+    }
+
     private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
     }
     private fun drawDifferenceClippingExample(canvas: Canvas) {
